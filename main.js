d0f4ba8f40dfb3dbafcd122816b8f32b39915406
Mr. Glass <saurav.uprety2000@gmail.com>
2023-12-18 16:50:35 +0545

    creats js file

---
+++ main.js
@@ -0,0 +1,31 @@
+Frontend technology
+
+html css 
+
+html css js
+
+frontend framework
+
+Vue.js, react js, angular js 
+
+css framework 
+
+bootstrap, tailwind
+
+
+What things are needed for js ?
+
+programming language ??
+
+variable 
+
+data types 
+
+control statement
+
+functions
+
+
+
+
+
